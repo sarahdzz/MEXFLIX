9a1dbeaf863350fe67c0db6810a83876d70c2283
sarahdzz <hdz.17sara@gmail.com>
2021-09-21 18:14:34 -0500

    2.Operaciones CRUD/20

--- crud.sql
+++ crud.sql
@@ -38,5 +38,59 @@
 	SELECT ms.imdb_id, ms.title, ms.plot, ms.author, ms.actors, ms.country, ms.premiere, ms.poster, ms.trailer, ms.rating, ms.genres, ms.category, s.status
 	FROM movieseries AS ms
     INNER JOIN status AS S
-    ON ms.status=s.status_id
+    ON ms.status=s.status_id;
     WHERE ms.status='1';
+
+/*status*/
+
+
+	/*Crear status*/
+	INSERT INTO status SET status_id = 0, status = 'Otro status';
+
+	/*Actualizar status*/
+	UPDATE status SET status = 'Other Status' WHERE status_id = 6;
+
+	/*Salvar status*/
+	REPLACE INTO status (status_id, status) VALUES (0, 'Otro Status');
+	REPLACE status SET status_id = 0, status = 'Otro Status';
+
+	/*Eliminar status*/
+	DELETE FROM status WHERE status_id = 6;
+
+	/*Buscar Todos los status*/
+	SELECT * FROM status;
+
+	/*Buscar un status por status_id*/
+	SELECT * FROM status WHERE status_id = 3;
+
+/*users*/
+
+	/*Crear user*/
+	INSERT INTO users SET user = '@usuario', email = 'usuario@midominio.com', name = 'Soy un usuario', birthday = '1988-10-09', pass = MD5('un_password'), role = 'Admin';
+
+	REPLACE users SET user = '@usuario', email = 'usuario@midominio.com', name = 'Soy un Usuario', birthday = '1988-09-08', pass = MD5('un_password'), role = 'Admin';
+
+	/*Actualizar*/
+		/*Datos generales*/
+		UPDATE users SET name = 'Soy un Usuario', birthday = '1984-10-09', role = 'User'
+			WHERE user = '@usuario' AND email = 'usuario@midominio.com';
+		/*Password*/
+		UPDATE users SET pass = MD5('un_nuevo_password')
+			WHERE user = '@usuario' AND email = 'usuario@midominio.com';
+
+		REPLACE users SET user = '@usuario', pass = MD5('un_nuevo_password');
+		REPLACE users SET user = '@usuario', email = 'usuario@midominio.com', name = 'Soy un Usuario', birthday = '1988-09-08', pass = MD5('un_nuevo_password'), role = 'Admin';
+
+	/*Eliminar user*/
+	DELETE FROM users WHERE user = '@usuario' AND email = 'usuario@midominio.com';
+   
+	/*Buscar Todos los users*/
+	SELECT * FROM users;
+
+	/*Buscar un user por:*/
+		/*user*/
+		SELECT * FROM users WHERE user = '@usuario';
+		/*email*/
+		SELECT * FROM users WHERE email = 'usuario@midominio.com';
+		/*role*/
+		SELECT * FROM users WHERE role = 'User';
