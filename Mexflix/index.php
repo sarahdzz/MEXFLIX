748bf408188c13f1bbf70d3b1ff6ac4fef1f7908
sarahdzz <hdz.17sara@gmail.com>
2021-09-23 20:29:20 -0500

    AUTOLOAD

--- Mexflix/index.php
+++ Mexflix/index.php
@@ -1,3 +1,6 @@
 <?php
+require_once('./Controllers/Autoload.php');
+$autoload = new Autoload();
+
 $route = (isset($_GET['r'])) ? $_GET['r'] : 'home'; //isst para saber si una variable esta definida o no
 $mexflix = new Router($route);
